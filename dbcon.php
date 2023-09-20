a922a78cd258a43ca9f7ecfd7709eb9a1a4dbfa0
elyon <elyonccari7692@gmail.com>
2023-09-19 20:53:44 -0500

    db

---
+++ dbcon.php
@@ -0,0 +1,9 @@
+<?php
+
+$con = mysqli_connect("localhost","root","Elyonccari","universitario");
+
+if(!$con){
+    die('Connexion fallida'. mysqli_connect_error());
+}
+
+?>
--- student-create.php
+++ student-create.php
@@ -5,7 +5,7 @@ session_start();
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+ 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
