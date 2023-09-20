011af53860ee9b5c89fe8b4f2b505e37d2d006c4
elyon <elyonccari7692@gmail.com>
2023-09-19 20:52:21 -0500

    dbco

--- student-create.php
+++ student-create.php
@@ -9,7 +9,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
+  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <title>Student Create</title>
